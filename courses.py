aafc202a054053f74aa8caee8119e1d4720be090
Timur <doxxerACM@gmail.com>
2013-11-11 17:13:46 +0400

    courses.py
    Список (словарь) курсов

---
+++ courses.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+# ID всех курсов, по которым ставятся оценки.
+# ID можно найти в URL к файлу
+# Например: https://docs.google.com/spreadsheet/ccc?key=0AtjSko0XIgLWdFZnSUdnTmhublJ6OWF5V2MybUJ0MHc&usp=drive_web#gid=0
+
+spreadsheets = {}
+spreadsheets['GT'] = "0AtjSko0XIgLWdFZnSUdnTmhublJ6OWF5V2MybUJ0MHc"
+spreadsheets['CO'] = "0AtjSko0XIgLWdHMzNjVSam5EdW52a0tNNTVXeVZtbmc"
