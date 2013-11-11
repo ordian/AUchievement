4fda613852bfabbfbc53cc33676e12de0a7746f9
andronik.ordian <andronik.ordian@gmail.com>
2013-11-11 19:08:13 +0400

    Algstructures added

--- parser.py
+++ parser.py
@@ -1,26 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import xlrd
 import datetime
+import openpyxl
 
-# Глобальные переменные - зло!
-GRAPH_LIST = [9,6,8]
-StInfoList = []
-
-def hw_number_graph(hw):
-    """
-    Возвращает номер домашнего задания
-    из порядкового номера задачи
-    """
-    accumulator = 0
-    counter = 1
-    for limit in GRAPH_LIST:
-        accumulator += limit
-        if hw < accumulator:
-            return counter
-        counter += 1
-    return None
 
 class StInfo(object):
     def __init__(self, name, surname, subject, hw, task, score, date):
@@ -33,32 +16,75 @@ class StInfo(object):
         self.date = date
 
     def __repr__(self):
-        s = "%s %s | %s/%s : %s" % (self.name, self.surname, self.hw, self.task, self.score)
+        s = "%s %s %s/%s \t\t %s \t\t%s" % (self.name, self.surname, self.hw, self.task, self.score, self.date)
         return s.encode("utf-8")
+
+
+GRAPH_LIST = [9, 6, 8]
+
+
+def hw_number(task, XXX_LIST):
+    """
+    Возвращает номер домашнего задания
+    из порядкового номера задачи
+    """
+    accumulator = 0
+    counter = 1
+    for limit in XXX_LIST:
+        accumulator += limit
+        if task < accumulator:
+            return counter
+        counter += 1
+    return None
+
 
 def to_float(num):
     try:
-        a = float(num)
-        return a
-    except Exception:
-        a = None
-    return a
+        return float(num)
+    except:
+        return None
 
-def parse_graph(file_graph):
-    """
-    Добавляет записи StInfo в StInfoList
-    """
-    global StInfoList, GRAPH_LIST
-    rb = xlrd.open_workbook(file_graph)
-    sheet = rb.sheet_by_index(0)
 
-    meta = sheet.row_values(0) # column names    
+def parse_graph(file):
+    """
+    Парсинг файла с оценками по теории графов
+    @param file: файл в формате OpenXML
+    """
+    workbook = openpyxl.load_workbook(filename=file)
     now = datetime.datetime.now()
-    for rownum in range(1, sheet.nrows):
-        row = sheet.row_values(rownum)
-        surname = row[0]
-        name = row[1]
-        for hw, score in enumerate(row[3:-2]):
-            StInfoList.append(StInfo(name, surname, u"Теория графов", hw_number_graph(int(hw)), meta[hw + 3], to_float(score), now))
+    for sheet in workbook.worksheets:
+        assert isinstance(sheet, openpyxl.worksheet.Worksheet)
+        meta = sheet.rows[0] # column names
+        print " ".join(unicode(x.value) for x in meta)
+        for row in sheet.rows[1:]:
+            surname = row[0].value
+            name = row[1].value
+            for hw, score in enumerate(row[3:-2]):
+                student = StInfo(name, surname, u"Теория графов", hw_number(int(hw), GRAPH_LIST), meta[hw + 3].value,
+                            to_float(score.value), now)
+                StInfoList.append(student)
 
 
+ALGSTRUCTURES_LIST = [10, 10, 10, 10]
+
+def parse_algstructures(file):
+    """
+    Парсинг файла с оценками по алгебраическим структурам
+    @param file: файл в формате OpenXML
+    """
+    workbook = openpyxl.load_workbook(filename=file)
+    now = datetime.datetime.now()
+    for sheet in workbook.worksheets:
+        assert isinstance(sheet, openpyxl.worksheet.Worksheet)
+        meta = sheet.rows[0] # column names
+        for row in sheet.rows[4:]:
+            surname, name = row[1].value.split()
+            for hw, score in enumerate(row[3:-8]):
+                student = StInfo(name, surname, u"Алгебраические структуры", hw_number(int(hw), ALGSTRUCTURES_LIST), str(int(meta[hw + 3].value)),
+                            to_float(score.value), now)
+                StInfoList.append(student)
+
+
+if __name__ == "__main__":
+    StInfoList = []
+    
