aa882b797a2601dcf70e2916348cf2a3b0681c29
Timur <doxxerACM@gmail.com>
2013-11-11 19:50:57 +0400

    Парсер GT + AL

--- parser.py
+++ parser.py
@@ -1,6 +1,4 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
+# coding=utf-8
 import datetime
 import openpyxl
 
@@ -20,24 +18,6 @@ class StInfo(object):
         return s.encode("utf-8")
 
 
-GRAPH_LIST = [9, 6, 8]
-
-
-def hw_number(task, XXX_LIST):
-    """
-    Возвращает номер домашнего задания
-    из порядкового номера задачи
-    """
-    accumulator = 0
-    counter = 1
-    for limit in XXX_LIST:
-        accumulator += limit
-        if task < accumulator:
-            return counter
-        counter += 1
-    return None
-
-
 def to_float(num):
     try:
         return float(num)
@@ -45,46 +25,98 @@ def to_float(num):
         return None
 
 
-def parse_graph(file):
+def hw_number(task, course_list):
     """
-    Парсинг файла с оценками по теории графов
-    @param file: файл в формате OpenXML
+    Возвращает номер домашнего задания
+    из порядкового номера задачи
     """
-    workbook = openpyxl.load_workbook(filename=file)
-    now = datetime.datetime.now()
-    for sheet in workbook.worksheets:
-        assert isinstance(sheet, openpyxl.worksheet.Worksheet)
-        meta = sheet.rows[0] # column names
-        print " ".join(unicode(x.value) for x in meta)
-        for row in sheet.rows[1:]:
-            surname = row[0].value
-            name = row[1].value
-            for hw, score in enumerate(row[3:-2]):
-                student = StInfo(name, surname, u"Теория графов", hw_number(int(hw), GRAPH_LIST), meta[hw + 3].value,
-                            to_float(score.value), now)
-                StInfoList.append(student)
+    accumulator = 0
+    counter = 1
+    for limit in course_list:
+        accumulator += limit
+        if task < accumulator:
+            return counter
+        counter += 1
+    return None
 
 
-ALGSTRUCTURES_LIST = [10, 10, 10, 10]
-
-def parse_algstructures(file):
+def AS_parse(file):
     """
     Парсинг файла с оценками по алгебраическим структурам
     @param file: файл в формате OpenXML
     """
+
+    AS_LIST = []
+
+    def build_hw_count(row):
+        """
+        Просматривает выданную строку и заполняет список AS_LIST - число заданий в каждой домашке
+        Заполнение делается в соответствии с правилом: если у новой ячейки есть правая жирная граница, то это конец домашки
+        @param row: строка, в которой это правило выполняется
+        """
+        counter = 0
+        for cell in row[3:-8]:
+            style = cell.style.borders.right.border_style
+            counter += 1
+            if style != 'none':
+                AS_LIST.append(counter)
+                counter = 0
+        AS_LIST.append(counter)
+        print AS_LIST
+
     workbook = openpyxl.load_workbook(filename=file)
     now = datetime.datetime.now()
     for sheet in workbook.worksheets:
-        assert isinstance(sheet, openpyxl.worksheet.Worksheet)
         meta = sheet.rows[0] # column names
+        build_hw_count(sheet.rows[0])
         for row in sheet.rows[4:]:
             surname, name = row[1].value.split()
             for hw, score in enumerate(row[3:-8]):
-                student = StInfo(name, surname, u"Алгебраические структуры", hw_number(int(hw), ALGSTRUCTURES_LIST), str(int(meta[hw + 3].value)),
-                            to_float(score.value), now)
+                student = StInfo(name, surname, u"Алгебраические структуры", hw_number(int(hw), AS_LIST),
+                                 str(int(meta[hw + 3].value)),
+                                 to_float(score.value), now)
+                StInfoList.append(student)
+
+
+def GT_parse(file):
+    """
+    Парсинг файла с оценками по теории графов
+    @param file: файл в формате OpenXML
+    """
+
+    GRAPH_LIST = [] # число заданий в каждой домашке.
+
+    def build_hw_count(row):
+        """
+        Просматривает выданную строку и заполняет список GRAPH_LIST - число заданий в каждой домашке
+        Заполнение делается в соответствии с правилом: если у новой ячейки есть правая жирная граница, то это конец домашки
+        @param row: строка, в которой это правило выполняется
+        """
+        counter = 0
+        for cell in row[3:-2]:
+            style = cell.style.borders.right.border_style
+            counter += 1
+            if style != 'none':
+                GRAPH_LIST.append(counter)
+                counter = 0
+
+
+    workbook = openpyxl.load_workbook(filename=file)
+    now = datetime.datetime.now()
+    for sheet in workbook.worksheets:
+        meta = sheet.rows[0] # column names
+        build_hw_count(sheet.rows[1])
+        for row in sheet.rows[1:]:
+            surname, name = row[0].value, row[1].value
+            for hw, score in enumerate(row[3:-2]):
+                student = StInfo(name, surname, u"Теория графов", hw_number(int(hw), GRAPH_LIST), meta[hw + 3].value,
+                                 to_float(score.value), now)
                 StInfoList.append(student)
 
 
 if __name__ == "__main__":
     StInfoList = []
-    
+    GT_parse("OpenXML/0AtjSko0XIgLWdFZnSUdnTmhublJ6OWF5V2MybUJ0MHc.xlsx")
+    AS_parse("OpenXML/0AhaIVBqbQZzvdElseEVUcGV5QV8tX1RzSnVIdnJoT0E.xlsx")
+    for st in StInfoList:
+        print st
