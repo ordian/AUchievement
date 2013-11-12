ba606367750fc8308c44217ece719aa9d2740eda
Timur <doxxerACM@gmail.com>
2013-11-12 20:30:39 +0400

    Updater
     Загрузчик данных в базу данных

--- Web/Achievement/views.py
+++ Web/Achievement/views.py
@@ -1,24 +1,38 @@
+import datetime
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.http import HttpResponse
 
-from Achievement.models import *
+from models import Student, Course, Mark
 
-def new_student(request):
-  student = Student()
-  student.name = "Semyon"
-  student.surname = "Atamas"
 
+def addStudents(nameList):
+    for t in nameList:
+        student = Student(first_name=t['name'], last_name=t['surname'])
+        student.save()
+
+
+def addCourses(courseList):
+    for i in courseList:
+        course = Course(course_name=i['subject'], course_code = i['code'])
+        course.save()
+
+
+def addMark(student, course, hw, task, date, score):
+    m = Mark(studentID=student, courseID=course, hwNo=hw, taskNo=task, date=date, result=score)
+    m.save()
 
 
 def dashboard(request):
-  return redirect('statistics/')
+    return redirect('statistics/')
+
 
 def people(request):
-  return render(request, 'Achievement/people.html')
+    return render(request, 'Achievement/people.html')
+
 
 def statistics(request):
-  return render(request, 'Achievement/statistics.html')
+    return render(request, 'Achievement/statistics.html')
+
 
 def achievements(request):
-  return render(request, 'Achievement/achievements.html')
+    return render(request, 'Achievement/achievements.html')
---
+++ updater.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+import logging
+import os
+import sys
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Web.Web.settings")
+sys.path.append('Web')
+from StudentInfo import StudentInfo
+from Web.Achievement.views import addMark
+from Web.Achievement.models import Student, Course, Mark
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def update(studentList):
+    studentList = list(studentList)
+    total = len(studentList)
+    for num, student_info in enumerate(studentList):
+        assert isinstance(student_info, StudentInfo)
+        if not student_info.score: continue
+
+        if num % 500 == 0:
+            print "{0}/{1}".format(num, total)
+
+        try:
+            student = Student.objects.get(first_name=student_info.name, last_name=student_info.surname)
+        except:
+            logging.error("student not found %s" % student_info)
+
+        try:
+            course = Course.objects.get(course_code=student_info.subject)
+        except:
+            logging.error("course not found %s" % student_info)
+
+        try:
+            mark = Mark.objects.get(studentID=student.id, courseID=course.id,
+                                    hwNo=student_info.hw, taskNo=student_info.task)
+
+            # если оценка нашлась, смотрим, изменилась ли она
+            new_mark = student_info.score
+            old_mark = mark.result
+            if old_mark != new_mark:
+                # если изменилась, пишем новую
+                mark.result = new_mark
+                mark.date = student_info.date
+                mark.save()
+        except ObjectDoesNotExist: # если такой оценки не было, то записываем ее
+            logging.info("writing new mark at student %s" % student_info)
+            addMark(student, course, student_info.hw, student_info.task, student_info.date, student_info.score)
