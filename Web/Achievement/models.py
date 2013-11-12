88e2ae32c728501a58a43dfc7ad3452883d131d0
Timur <doxxerACM@gmail.com>
2013-11-12 23:47:31 +0400

    change model (rename column)

--- Web/Achievement/models.py
+++ Web/Achievement/models.py
@@ -1,25 +1,30 @@
 from django.db import models
 
+
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+
 class Course(models.Model):
-    course_name=models.CharField(max_length=100)
-    course_code=models.CharField(max_length=10)
+    course_name = models.CharField(max_length=100)
+    course_code = models.CharField(max_length=10)
+
 
 class Mark(models.Model):
-    studentID=models.ForeignKey(Student)
-    courseID=models.ForeignKey(Course)
-    hwNo=models.IntegerField()
-    taskNo=models.CharField(max_length=20)
-    date=models.DateTimeField()
-    result=models.FloatField()
+    studentID = models.ForeignKey(Student)
+    courseID = models.ForeignKey(Course)
+    hwNo = models.IntegerField()
+    taskNo = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    result = models.FloatField()
+
 
 class Achievement(models.Model):
-    achievement=models.CharField(max_length=100)
-    description=models.CharField(max_length=500)
+    code = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
 
 class AchievedAchievement(models.Model):
-    achievementID=models.ForeignKey(Achievement)
-    studentID=models.ForeignKey(Student)
+    achievementID = models.ForeignKey(Achievement)
+    studentID = models.ForeignKey(Student)
