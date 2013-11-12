049682807625fbde8dec184b1609a2693105628b
Timur <doxxerACM@gmail.com>
2013-11-12 20:30:21 +0400

    Initial fill
    Загрузка базы начальными значениями

---
+++ initial_fill.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+studentList = [{'surname': u'Аманов', 'name': u'Карим'},
+               {'surname': u'Атамась', 'name': u'Семен'},
+               {'surname': u'Афанасьев', 'name': u'Антон'},
+               {'surname': u'Бубнов', 'name': u'Никита'},
+               {'surname': u'Бугаев', 'name': u'Богдан'},
+               {'surname': u'Ворончихин', 'name': u'Станислав'},
+               {'surname': u'Жарков', 'name': u'Денис'},
+               {'surname': u'Калакуцкий', 'name': u'Аркадий'},
+               {'surname': u'Карташов', 'name': u'Никита'},
+               {'surname': u'Коваленко', 'name': u'Владимир'},
+               {'surname': u'Комаров', 'name': u'Александр'},
+               {'surname': u'Крыщенко', 'name': u'Антон'},
+               {'surname': u'Лучихин', 'name': u'Кирилл'},
+               {'surname': u'Михайленко', 'name': u'Дмитрий'},
+               {'surname': u'Моисеева', 'name': u'Анастасия'},
+               {'surname': u'Новокрещенов', 'name': u'Константин'},
+               {'surname': u'Обедин', 'name': u'Николай'},
+               {'surname': u'Овчинников', 'name': u'Даниил'},
+               {'surname': u'Ордиян', 'name': u'Андроник'},
+               {'surname': u'Разметов', 'name': u'Сергей'},
+               {'surname': u'Тураев', 'name': u'Марат'},
+               {'surname': u'Тураев', 'name': u'Тимур'},
+               {'surname': u'Устюжанина', 'name': u'Екатерина'},
+               {'surname': u'Фетцер', 'name': u'Юрий'},
+               {'surname': u'Хабибуллин', 'name': u'Марат'},
+               {'surname': u'Цветков', 'name': u'Алексей'},
+               {'surname': u'Авдеев', 'name': u'Павел'},
+               {'surname': u'Бондарев', 'name': u'Тимофей'},
+               {'surname': u'Гайдай', 'name': u'Игорь'},
+               {'surname': u'Демидов', 'name': u'Герман'},
+               {'surname': u'Лиознова', 'name': u'Анна'},
+               {'surname': u'Малыгина', 'name': u'Татьяна'},
+               {'surname': u'Мелешко', 'name': u'Дмитрий'},
+               {'surname': u'Сидоров', 'name': u'Святослав'},
+               {'surname': u'Ситдыкова', 'name': u'Надия'},
+               {'surname': u'Смолина', 'name': u'Ирина'},
+               {'surname': u'Старостина', 'name': u'Екатерина'},
+               {'surname': u'Яснев', 'name': u'Олег'}]
+
+courseList = [{'code': 'GT', 'subject': u"Теория графов"},
+              {'code': 'CO', 'subject': u"Комбинаторика"},
+              {'code': 'AS', 'subject': u"Алгебраические структуры"},
+              {'code': 'AL1', 'subject': u"Алгоритмы (подгруппа Опарина)"},
+              {'code': 'AL2', 'subject': u"Алгоритмы (подгруппа Фондаратова)"},
+              {'code': 'ML1', 'subject': u"Математическая логика (подгруппа Москвина)"},
+              {'code': 'ML2', 'subject': u"Математическая логика (подгруппа Исаева)"},
+              {'code': 'UX', 'subject': u"Unix и скриптовые языки"}]
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Web.Web.settings")
+
+from Web.Achievement.views import addCourses
+from Web.Achievement.views import addStudents
+
+addStudents(studentList)
+addCourses(courseList)
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+# coding=utf-8
 import logging
 from downloader import download
 from parser import parse
+from updater import update
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
+    # logging.basicConfig(filename="main.log", format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
+
+    logging.basicConfig(filename="error.log", format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
+                        level=logging.ERROR)
+
     logging.info("--------------------begin downloading----------------------")
     download()
     logging.info("---------------------end downloading----------------------")
 
+
     logging.info("--------------------begin parsing----------------------")
-    parse()
+    list = parse()
     logging.info("---------------------end parsing----------------------")
+
+
+    logging.info("--------------------begin update----------------------")
+    update(list)
+    logging.info("---------------------end update----------------------")
