a3b55cfb6a2c40d4559688bc8452deb5723cd2d4
Timur <doxxerACM@gmail.com>
2013-11-13 00:08:13 +0400

    new achieve hot_n
    Ачивка вычисляет N топов по каждому курсу

--- Achiever.py
+++ Achiever.py
@@ -2,6 +2,7 @@
 
 from achievers.full_hw import full_hw_achiever
 from achievers.hot_n import hot_n_achiever
+from achievers.top_n import top_n_achiever
 import courses
 from timer import timer
 
@@ -9,9 +10,8 @@ from timer import timer
 @timer
 def achieve():
     for course in courses.spreadsheets.keys():
-        for solved_count in range(5, 110, 10):
-            print course, solved_count
-            hot_n_achiever(solved_count, course)
-
-    for course in courses.spreadsheets.keys():
+        for solved_count in range(0, 100, 10):
+            print course, solved_count + 10
+            hot_n_achiever(course, solved_count + 10)
         full_hw_achiever(course)
+        top_n_achiever(course, 3)
---
+++ achievers/top_n.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+
+import operator
+import os
+import sys
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Web.Web.settings")
+sys.path.append('Web')
+from Web.Achievement.models import Mark, Course, Student, Achievement, AchievedAchievement
+
+
+def top_n_achiever(course_code, top_num):
+    """
+    Топ top_num студентов по предмету courseCode
+    @param course_code: предмет по которому строится топ
+    @param top_num: нужное число студентов в топе
+    """
+    achieve_code = "{0}_{1}_{2}".format("TOP", top_num, course_code)
+    achieve = Achievement.objects.get_or_create(code=achieve_code)[0]
+    course_id = Course.objects.get(course_code=course_code).pk
+
+    score_sum = {}
+    student_dict = {}
+    for student in Student.objects.all():
+        student_dict[student.pk] = student
+        score_sum[student.pk] = 0
+    for mark in Mark.objects.filter(courseID=course_id):
+        score_sum[mark.studentID.pk] += mark.result
+
+    sorted_sum = sorted(score_sum.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+    for student_id, value in sorted_sum[:top_num]:
+        AchievedAchievement.objects.get_or_create(achievementID=achieve, studentID=student_dict[student_id])
