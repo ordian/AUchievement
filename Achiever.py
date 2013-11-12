d2aa6340394a0d293066f22d91865cea416b70fc
satamas <se.atamas@gmail.com>
2013-11-12 22:46:33 +0400

    One more achievement type

--- Achiever.py
+++ Achiever.py
@@ -6,8 +6,24 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Web.Web.settings")
 sys.path.append('Web')
 from Web.Achievement.models import Mark, Course, Student, Achievement, AchievedAchievement
 
+def whole_hw_achiever(courseCode, achieve_name, hwN):
+    achieve = Achievement.objects.get_or_create(achievement = achieve_name, defaults ={})[0]
+    course = Course.objects.get(course_code=courseCode)
 
-def AS_achiever( limit , courseCode, achieve_name):
+    for student in Student.objects.all():
+        marks=Mark.objects.filter(courseID = course, studentID=student, hwNo = hwN)
+        allHW = True
+        if (marks.__len__() != 0):
+            for mark in marks:
+                if mark.result==0:
+                    allHW = False
+        else:
+            allHW = False
+        if allHW:
+            AchievedAchievement.objects.get_or_create( achievementID = achieve, studentID = student,  defaults={})
+
+
+def course_total_achiever( limit , courseCode, achieve_name):
     achieve = Achievement.objects.get_or_create(achievement = achieve_name, defaults ={})[0]
     course = Course.objects.get(course_code=courseCode)
     summ={}
@@ -19,5 +35,5 @@ def AS_achiever( limit , courseCode, achieve_name):
         if summ[id] >= limit:
             AchievedAchievement.objects.get_or_create( achievementID = achieve , studentID = Student.objects.get(pk = id), defaults={})
 
-limit=4
-AS_achiever(4, 'AS', 'hot'+str(limit))
+hwN=1
+whole_hw_achiever('AS', 'allHot', hwN)
