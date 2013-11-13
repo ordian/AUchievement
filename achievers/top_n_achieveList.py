0c075114288f3eefdf3ab2467b0e791464e5ccb9
ordian <ordian@users.noreply.github.com>
2013-11-13 13:28:04 +0300

    Update top_n_achieveList.py

--- achievers/top_n_achieveList.py
+++ achievers/top_n_achieveList.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 
 achieve_list = {}
-achieve_list["GT"]    = "Графоман"
-achieve_list["CO"]    = "Великий комбинатор"
-achieve_list["AS"]    = "Что-то я поГорячился"
+achieve_list["GT"] = "Графоман"
+achieve_list["CO"] = "Великий комбинатор"
+achieve_list["AS"] = "Что-то я поГорячился"
 achieve_list["AL1SE"] = "АЛГОЛитмист"
 achieve_list["AL1BI"] = "АЛГОЛитмист"
-achieve_list["AL2"]   = "АЛГОЛитмист"
-achieve_list["ML1"]   = "Логичный"
-achieve_list["ML2"]   = "Логичный"
-achieve_list["UX"]    = "Питонист-башолюб"
+achieve_list["AL2"] = "АЛГОЛитмист"
+achieve_list["ML1"] = "Последователь Гёделя"
+achieve_list["ML2"] = "Последователь Гёделя"
+achieve_list["UX"] = "Питонист-башолюб"
