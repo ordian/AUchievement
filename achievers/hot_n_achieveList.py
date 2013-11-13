3d674f4a56d959b43f62ef3a96181bc9eac6bcb4
ordian <ordian@users.noreply.github.com>
2013-11-13 13:40:27 +0300

    Create hot_n_achieveList.py

---
+++ achievers/hot_n_achieveList.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+
+# ... на N процентов
+achieve_list = {}
+achieve_list["GT"] = "Закинулся амфетаминами"
+achieve_list["CO"] = "Перечислил берёзки"
+achieve_list["AS"] = "Проникся"
+achieve_list["AL1SE"] = "Рекурсивен"
+achieve_list["AL1BI"] = "Геном собран"
+achieve_list["AL2"] = "Рекурсивен"
+achieve_list["ML1"] = "Железная логика"
+achieve_list["ML2"] = "Железная логика"
+achieve_list["UX"] = "Магия прокачана"
