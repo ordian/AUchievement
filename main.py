d3ebfbc847c351357af70c6b6a902012ba367395
satamas <se.atamas@gmail.com>
2013-11-13 13:54:06 +0400

    Repeatable script

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 # coding=utf-8
 import logging
-from achiever import achieve
+#from achiever import achieve
 from downloader import download
 from parser import parse
 from updater import update
+import time
+
 
 
 if __name__ == "__main__":
     # logging.basicConfig(filename="main.log", format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
+    def do_something(sleep_time):
+            print 1
 
-    logging.basicConfig(filename="error.log", format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
-                        level=logging.ERROR)
+            logging.basicConfig(filename="error.log", format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
+            level=logging.ERROR)
 
-    logging.info("--------------------begin downloading----------------------")
-    download()
-    logging.info("---------------------end downloading----------------------")
-
-
-    logging.info("--------------------begin parsing----------------------")
-    list = parse()
-    logging.info("---------------------end parsing----------------------")
+            logging.info("--------------------begin downloading----------------------")
+            #download()
+            logging.info("---------------------end downloading----------------------")
 
 
-    logging.info("--------------------begin update----------------------")
-    update(list)
-    logging.info("---------------------end update----------------------")
+            logging.info("--------------------begin parsing----------------------")
+            #list = parse()
+            logging.info("---------------------end parsing----------------------")
 
-    logging.info("--------------------begin achiever----------------------")
-    achieve()
-    logging.info("---------------------end achiever----------------------")
+
+            logging.info("--------------------begin update----------------------")
+            #update(list)
+            logging.info("---------------------end update----------------------")
+
+            logging.info("--------------------begin achiever----------------------")
+            #achieve()
+            logging.info("---------------------end achiever----------------------")
+
+            time.sleep(sleep_time)
+
+    while(True):
+        do_something(15)
+
