5282a37816b97de8a07f0022aff784651c63be2e
ordian <ordian@users.noreply.github.com>
2013-11-13 10:36:52 +0300

    Update ML2_parser.py

--- parsers/ML2_parser.py
+++ parsers/ML2_parser.py
@@ -2,17 +2,20 @@
 # -*- coding: utf-8 -*-
 
 
-from MetaParser import *
+import openpyxl
 from StudentInfo import StudentInfo
+import parser
 
 
 def simple_score(cell):
     return int(cell.style.fill.start_color.index == "FF00FF00")
 
-
-def parse(fileOpenXML, time, studentList):
+#WARNING: not tested yet
+def parse(fileOpenXML, time):
     """
     HARDCORE
+    Каждое домашнее задание имеет 1 задачу
+    Задача зачтена, если цвет зелёный
     """
     subject = u"ML2"
     workbook = openpyxl.load_workbook(filename=fileOpenXML)
@@ -21,5 +24,5 @@ def parse(fileOpenXML, time, studentList):
         for row in sheet.rows[1:]:
             surname, name = row[0].value.split()
             for task, score in enumerate(row[1:8]):
-                student = StudentInfo(name, surname, subject, 1 + int(task), 1 + int(task), simple_score(score), time)
-                studentList.append(student)
+                student = StudentInfo(name, surname, subject, 1 + int(task), 1, simple_score(score), time)
+                StudentList.append(student)
