672f407f94b9de29de64a7ceca562da190df3b63
ordian <ordian@users.noreply.github.com>
2013-11-13 11:58:23 +0300

    SE/BI

--- parsers/AL1_parser.py
+++ parsers/AL1_parser.py
@@ -6,16 +6,17 @@ from MetaParser import *
 
 
 def parse(fileOpenXML, time):
-    subject = u"AL1"
+    subject_SE = u"AL1SE"
+    subject_BI = u"AL1BI"
     skip = 1
     stop = 16
     getname = lambda row: (row[1].value, row[2].value)
     leftborder = 5
     rightborder = None
-    metaparse(fileOpenXML, subject, skip, stop, getname,
+    metaparse(fileOpenXML, subject_SE, skip, stop, getname,
               leftborder, rightborder, to_float, time, 0, 1)
     skip = 2
     stop = 14
-    metaparse(fileOpenXML, subject, skip, stop, getname,
+    metaparse(fileOpenXML, subject_BI, skip, stop, getname,
               leftborder, rightborder, to_float, time, 1)
    
