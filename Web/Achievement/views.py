0dc9635284a3766ae6266a63af7229a58820b3ce
Timur <doxxerACM@gmail.com>
2013-11-13 11:25:24 +0400

    minor reformat

--- Web/Achievement/views.py
+++ Web/Achievement/views.py
@@ -8,6 +8,7 @@ from django.db import connection
 
 from models import Student, Course, Mark, AchievedAchievement, Achievement
 
+
 def addStudents(nameList):
     for t in nameList:
         student = Student(first_name=t['name'], last_name=t['surname'])
@@ -16,7 +17,7 @@ def addStudents(nameList):
 
 def addCourses(courseList):
     for i in courseList:
-        course = Course(course_name=i['subject'], course_code = i['code'])
+        course = Course(course_name=i['subject'], course_code=i['code'])
         course.save()
 
 
@@ -42,54 +43,54 @@ def statistics(request, id):
 
     courses_list = []
     for mark in marks:
-      course_name = mark.courseID.course_name
-      if course_name not in course_mark:
-        course_mark[course_name] = dict()
-        courses_list.append(course_name)
-      
-      if mark.hwNo not in course_mark[course_name]:
-        course_mark[course_name][mark.hwNo] = 0
+        course_name = mark.courseID.course_name
+        if course_name not in course_mark:
+            course_mark[course_name] = dict()
+            courses_list.append(course_name)
 
-      course_mark[course_name][mark.hwNo] += mark.result
+        if mark.hwNo not in course_mark[course_name]:
+            course_mark[course_name][mark.hwNo] = 0
+
+        course_mark[course_name][mark.hwNo] += mark.result
 
     courses_list.sort()
     homework_completion = dict()
     for course in courses_list:
-      homework_completion[course] = {
-        'homeworks': list(sorted(course_mark[course].keys())), 
-        'completion': course_mark[course]
-      }
+        homework_completion[course] = {
+            'homeworks': list(sorted(course_mark[course].keys())),
+            'completion': course_mark[course]
+        }
 
     courses = Course.objects.all();
     course_overall = dict()
     for course in courses:
-      cursor = connection.cursor()
-      cursor.execute(
-        '''SELECT SUM(course_marks.task_max_result)
+        cursor = connection.cursor()
+        cursor.execute(
+            '''SELECT SUM(course_marks.task_max_result)
         FROM
         (SELECT taskNo, MAX(result) as task_max_result
         FROM achievement_mark 
         WHERE courseID_id=%s 
-        GROUP BY taskNo) course_marks''', 
-        [course.id])
-      total = cursor.fetchone()[0]
+        GROUP BY taskNo) course_marks''',
+            [course.id])
+        total = cursor.fetchone()[0]
 
-      student_marks = Mark.objects.filter(studentID__exact=student.id)
-      course_marks = student_marks.filter(courseID__exact=course.id)
-      current = course_marks.aggregate(Sum('result')).values()[0]
-      if current is None:
-        percent = 0
-      else:
-        percent_f = round(current/total*100)
-        percent = min(int(percent_f), 100)
+        student_marks = Mark.objects.filter(studentID__exact=student.id)
+        course_marks = student_marks.filter(courseID__exact=course.id)
+        current = course_marks.aggregate(Sum('result')).values()[0]
+        if current is None:
+            percent = 0
+        else:
+            percent_f = round(current / total * 100)
+            percent = min(int(percent_f), 100)
 
-      course_overall[course.course_name] = percent
+        course_overall[course.course_name] = percent
 
     data = {
-      'courses': courses_list, 
-      'activities': homework_completion,
-      'overall': course_overall,
-      'name': student.first_name + ' ' + student.last_name
+        'courses': courses_list,
+        'activities': homework_completion,
+        'overall': course_overall,
+        'name': student.first_name + ' ' + student.last_name
     }
 
     return render(request, 'Achievement/statistics.html', data)
@@ -102,29 +103,29 @@ def achievements(request, id):
 
     badges = []
     for ach in achievements:
-      badge = dict()
-      badge['image'] = ach.image
-      
-      code = ach.code
-      code = code.replace('FULL_HW', 'FULLHW')
+        badge = dict()
+        badge['image'] = ach.image
 
-      a_type, param, corse_code = code.split('_')
+        code = ach.code
+        code = code.replace('FULL_HW', 'FULLHW')
 
-      if a_type == 'FULLHW':
-        badge['name'] = u'Полное ДЗ %s' % (param,)
-      elif a_type == 'HOT':
-        badge['name'] = u'Решил %s задач' % (param,)
-      elif a_type == 'TOP':
-        badge['name'] = u'Топ %s' % (param,)
+        a_type, param, course_code = code.split('_')
 
-      course_name = Course.objects.get(course_code__exact=corse_code).course_name
-      badge['description'] = course_name
+        if a_type == 'FULLHW':
+            badge['name'] = u'Полное ДЗ %s' % (param,)
+        elif a_type == 'HOT':
+            badge['name'] = u'Решил %s задач' % (param,)
+        elif a_type == 'TOP':
+            badge['name'] = u'Топ %s' % (param,)
 
-      badges.append(badge)
+        course_name = Course.objects.get(course_code__exact=course_code).course_name
+        badge['description'] = course_name
+
+        badges.append(badge)
 
     data = {
-      'achievements': badges,
-      'name': student.first_name + ' ' + student.last_name
+        'achievements': badges,
+        'name': student.first_name + ' ' + student.last_name
     }
 
     return render(request, 'Achievement/achievements.html', data)
