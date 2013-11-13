05a8571a5bd5b5540be590cd4ddf21efbb926d1a
ordian <ordian@users.noreply.github.com>
2013-11-13 10:52:11 +0300

    NOT TESTED

--- parsers/UX_parser.py
+++ parsers/UX_parser.py
@@ -1,43 +1,18 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from MetaParser import *
 
-
-def deal_with_kuzya(sheet, task):
-    """
-    Magic
-    """
-    if task < 13:
-        return "Bash {0}".format(task)
-    else:
-        return "Python {0}".format(int(task) - 12)
-
-
-def UX_parse(fileOpenXML):
-    """
-    Not tested yet
-    """
+#WARNING! NOT TESTED YET
+def parse(fileOpenXML, time):
     subject = u"UX"
     skip = 5
     stop = 31
     getname = lambda row: (row[1].value, row[2].value)
     leftborder = 10
     rightborder = None
+    convert = to_float
+    hw_number_l = lambda task, offset: (task < 12 and "Bash") or (1 and "Python")
     metaparse(fileOpenXML, subject, skip, stop, getname,
-              leftborder, rightborder, to_float, deal_with_kuzya, now)
-
-
-if __name__ == "__main__":
-#    StInfoList = []
-#    folder = "OpenXML"
-#    ext = "xlsx"#
-#
-    import datetime
-
-    now = datetime.datetime.now()
-#
-#    UX_parse("{0}.{1}".format(os.path.join(folder, courses.spreadsheets['UX']), ext))
-# 
-#    for st in StInfoList: print st
+              leftborder, rightborder, convert, time, hw_number_l)
+   
