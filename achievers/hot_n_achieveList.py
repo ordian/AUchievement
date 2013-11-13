3a212ec52ecdbb714d1361dd60d54aee61e65ce4
ordian <ordian@users.noreply.github.com>
2013-11-13 13:48:20 +0300

    Update hot_n_achieveList.py

--- achievers/hot_n_achieveList.py
+++ achievers/hot_n_achieveList.py
@@ -1,14 +1,13 @@
 # coding=utf-8
 
 
-# ... на N процентов
 achieve_list = {}
-achieve_list["GT"] = "Закинулся амфетаминами"
-achieve_list["CO"] = "Перечислил берёзки"
-achieve_list["AS"] = "Проникся"
-achieve_list["AL1SE"] = "Рекурсивен"
-achieve_list["AL1BI"] = "Геном собран"
-achieve_list["AL2"] = "Рекурсивен"
-achieve_list["ML1"] = "Железная логика"
-achieve_list["ML2"] = "Железная логика"
-achieve_list["UX"] = "Магия прокачана"
+achieve_list["GT"] = "Закинулся амфетаминами на $ процентов"
+achieve_list["CO"] = "Вычислил число Рамсея R($,$)"
+achieve_list["AS"] = "Проникся на $ процентов"
+achieve_list["AL1SE"] = "Рекурсивен на $ процентов"
+achieve_list["AL1BI"] = "Геном собран на $ процентов"
+achieve_list["AL2"] = "Рекурсивен на $ процентов"
+achieve_list["ML1"] = "Железная логика на $ процентов"
+achieve_list["ML2"] = "Железная логика на $ процентов"
+achieve_list["UX"] = "Магия прокачана на $ процентов"
