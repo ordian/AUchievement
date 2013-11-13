9456cf5fb8eea492e653066278cf20fba348758c
Timur <doxxerACM@gmail.com>
2013-11-13 12:30:37 +0400

    ML2 parser fix

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import datetime
 import os
 
 import courses
-from parsers import AL1_parser, AL2_parser, CO_parser, GT_parser, AS_parser, ML1_parser
+from parsers import AL1_parser, AL2_parser, CO_parser, GT_parser, AS_parser, ML1_parser, ML2_parser
 from timer import timer
 
 
@@ -21,7 +21,7 @@ def parse():
     AL1_file = "{0}.{1}".format(os.path.join(folder, courses.spreadsheets['AL1']), ext)
     AL2_file = "{0}.{1}".format(os.path.join(folder, courses.spreadsheets['AL2']), ext)
     ML1_file = "{0}.{1}".format(os.path.join(folder, courses.spreadsheets['ML1']), ext)
-    # ML2_file = "{0}.{1}".format(os.path.join(folder, courses.spreadsheets['ML2']), ext)
+    ML2_file = "{0}.{1}".format(os.path.join(folder, courses.spreadsheets['ML2']), ext)
 
     GT_parser.parse(GT_file, time)
     CO_parser.parse(CO_file, time)
@@ -29,8 +29,6 @@ def parse():
     AL1_parser.parse(AL1_file, time)
     AL2_parser.parse(AL2_file, time)
     ML1_parser.parse(ML1_file, time)
+    ML2_parser.parse(ML2_file, time)
 
     return StudentInfoList
-
-
-    # ML2_parser.parse(ML2_file, time) bug!
--- parsers/ML2_parser.py
+++ parsers/ML2_parser.py
@@ -10,7 +10,7 @@ import parser
 def simple_score(cell):
     return int(cell.style.fill.start_color.index == "FF00FF00")
 
-#WARNING: not tested yet
+
 def parse(fileOpenXML, time):
     """
     HARDCORE
@@ -23,10 +23,12 @@ def parse(fileOpenXML, time):
         assert isinstance(sheet, openpyxl.worksheet.Worksheet)
         for row in sheet.rows[1:]:
             surname, name = row[0].value.split()
+            name = name.replace(u'ё', u'е')
+            surname = surname.replace(u'ё', u'е')
             for task, score in enumerate(row[1:]):
                 try:
                     task_num = int(task) + 1
                     student = StudentInfo(name, surname, subject, task_num, 1, simple_score(score), time)
-                    StudentList.append(student)
+                    parser.StudentInfoList.append(student)
                 except:
                     break # ДЗ кончились
